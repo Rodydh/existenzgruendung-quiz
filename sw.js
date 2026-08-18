c440b5aa1615a0b499ec61916bd6494deaf5b799
Rodydh <ismailrody01@gmail.com>
2026-08-18 20:21:10 +0200

    Add offline support

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='gruendungsquiz-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./questions-1.js','./questions-2.js','./questions-3.js','./questions-4.js','./questions-4a.js','./questions-4b.js','./questions-4c.js','./questions-4d.js','./questions-5.js','./questions-5a.js','./questions-5b.js','./questions-5c.js','./questions-5d.js','./app.js','./app1.js','./app2.js'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return resp;}).catch(()=>caches.match('./index.html'))));});
